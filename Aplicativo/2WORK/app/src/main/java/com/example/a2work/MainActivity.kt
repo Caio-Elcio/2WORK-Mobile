c5853ea9ffab54a64b499e6714c4073af3dba893
MauSantana <mauricio.santos@bandtec.com.br>
2022-02-25 13:37:54 -0300

    telas de home, login e cadastro estáticas

---
+++ Aplicativo/2WORK/app/src/main/java/com/example/a2work/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.a2work
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.inicio)
+    }
+}
