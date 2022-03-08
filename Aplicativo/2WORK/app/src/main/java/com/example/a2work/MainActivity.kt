bbde40cc5b46b308d6560c62dd7c43e702d713a2
Caio-Elcio <caio.eloy@bandtec.com.br>
2022-03-08 10:26:51 -0300

    Update project

--- Aplicativo/2WORK/app/src/main/java/com/example/a2work/MainActivity.kt
+++ Aplicativo/2WORK/app/src/main/java/com/example/a2work/MainActivity.kt
@@ -6,10 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
         setContentView(R.layout.projetos)
-=======
-        setContentView(R.layout.upload)
->>>>>>> 96346e98cb891e6e78660e47823c5d9f4d92aa52
     }
 }
