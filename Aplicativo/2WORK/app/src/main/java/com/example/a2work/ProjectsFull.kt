6f32a870e9bfe4df03695451f92e62f6e9b98607
lucasgfelix <lucas.felix@bandtec.com.br>
2022-05-13 19:27:00 -0300

    carrosel tela de projeto

--- Aplicativo/2WORK/app/src/main/java/com/example/a2work/ProjectsFull.kt
+++ Aplicativo/2WORK/app/src/main/java/com/example/a2work/ProjectsFull.kt
@@ -2,10 +2,32 @@ package com.example.a2work
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
 
 class ProjectsFull : AppCompatActivity() {
+
+    var imageArray: ArrayList<Int> = ArrayList()
+    var carouselView: CarouselView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_projects_full)
+
+        imageArray.add(R.drawable.image_1)
+        imageArray.add(R.drawable.image_2)
+        imageArray.add(R.drawable.image_3)
+        imageArray.add(R.drawable.image_4)
+
+        carouselView = findViewById(R.id.carouselView)
+
+        carouselView!!.pageCount = imageArray.size
+
+        carouselView!!.setImageListener(imageListener)
+
     }
+
+
+    var imageListener =
+        ImageListener { position, imageView -> imageView.setImageResource(imageArray[position]) }
 }
